e171ff25a2ccd6a6a6424d0892796bae68ec47ba
Danilo Franeta <franetadanilo2@gmail.com>
2026-03-18 02:17:35 +0100

    Pokusaj jos jedan fiskiranja

--- script.js
+++ script.js
@@ -172,3 +172,11 @@ function setRendererSize() {
 
 window.addEventListener('resize', setRendererSize);
 setRendererSize();
+
+function setVh() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', setVh);
+setVh();
